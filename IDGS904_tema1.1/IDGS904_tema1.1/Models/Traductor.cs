4560e8fbf0c25c8e3e71ef9fb6603a12c789e34f
IDGS-904-19002275 <122642827+IDGS-904-19002275@users.noreply.github.com>
2023-05-23 02:17:34 -0600

    LoDelTraductor

---
+++ IDGS904_tema1.1/IDGS904_tema1.1/Models/Traductor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDGS904_tema1._1.Models
+{
+    public class Traductor
+    {
+        public String ESP { get; set; }
+        public String ING { get; set; }
+    
+        public String PALABRA { get; set; }
+        public int IDIOMA { get; set; }
+
+    }
+    //public class TraductorGuardar
+    //{
+    //    public String ESP { get; set; }
+    //    public String ING { get; set; }
+    //}
+    //public class TraductorConsultar
+    //{
+    //    public String PALABRA { get; set; }
+    //    public int IDIOMA { get; set; }
+
+    //}
+}
