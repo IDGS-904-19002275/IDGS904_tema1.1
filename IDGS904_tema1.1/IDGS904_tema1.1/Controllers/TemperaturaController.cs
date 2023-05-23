0018db13ad5df116737d0d0730e7c09a805010b0
IDGS-904-19002275 <122642827+IDGS-904-19002275@users.noreply.github.com>
2023-05-23 03:16:38 -0600

    Temperatura

---
+++ IDGS904_tema1.1/IDGS904_tema1.1/Controllers/TemperaturaController.cs
@@ -0,0 +1,37 @@
+﻿using IDGS904_tema1._1.Models;
+using IDGS904_tema1._1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace IDGS904_tema1._1.Controllers
+{
+    public class TemperaturaController : Controller
+    {
+        public ActionResult TemperaturaForm()
+        {
+              return View();
+            
+        }
+        public RedirectToRouteResult TemperaturaProceso(Temperatura T)
+        {
+            var model = new Temperatura();
+            TempData["TemperatraConvertida"] = model.Convertir(T.TempeaturaInicio, T.EscalaAConvertir);
+            return RedirectToAction("TemperaturaText");            
+        }
+        public ActionResult TemperaturaText()
+        {
+
+            String tem = "No hay temperatura, regrese al formulario";
+            if (TempData.ContainsKey("TemperatraConvertida"))
+            {
+                tem = TempData["TemperatraConvertida"] as string;
+            }
+            ViewBag.Fin = tem;
+            return View();
+        }
+    }
+}
